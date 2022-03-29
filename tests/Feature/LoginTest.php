e7302230f7a71432a72be5027c14a689041b0313
Felipe Crosa <felipe@lightit.com>
2022-03-29 15:10:48 -0300

    Test for invalid and wrong login

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -21,4 +21,53 @@ class LoginTest extends TestCase
         $response->assertSuccessful();
         $response->assertJson(['status'=>200, 'message'=>'User logged in succesfully']);
     }
+
+    /**
+     * @dataProvider wrongCredentialsDataProvider
+     */
+    public function test_users_log_in_with_wrong_credentials($user)
+    {
+        $createdUser = User::factory()->create(['email'=>'felipe@lightit.io', 'password'=>'1234567']);
+        $response = $this->postJson('/api/login', $user);
+
+        $response->assertJson(['status'=>401, 'message'=>'Invalid credentials']);
+    }
+
+    /**
+     * @dataProvider invalidUserDataProvider
+     */
+    public function test_login_with_invalid_user_data($user)
+    {
+        $response = $this->postJson('/api/login', $user);
+
+        $response->assertStatus(422);
+    }
+
+    public function wrongCredentialsDataProvider()
+    {
+        return [
+            ['wrong Password'=>[
+                'email'=>'felipe@lightit.io',
+                'password'=>'12344643',
+            ]],
+            ['wrong Email'=>[
+                'email'=>'juan@lightit.io',
+                'password'=>'1234626526',
+            ]],
+        ];
+    }
+
+    public function invalidUserDataProvider()
+    {
+        return [
+            ['wrong email'=>[
+                'email'=>'felipelightitio',
+                'password'=>'12344643',
+            ]],
+            ['wrong password'=>[
+                'email'=>'juan@lightit.io',
+                'password'=>'1234',
+            ]],
+        ];
+    }
 }
