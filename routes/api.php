cce1379c55470e7c6d06d962e0bd17303d908957
felipe-crosa <felipe-crosa@users.noreply.github.com>
2022-03-30 17:08:30 +0000

    Fix styling

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
-
-
 Route::post('/login', \App\Http\Controllers\UserLoginController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
