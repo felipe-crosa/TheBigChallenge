7725f66b6e7eada80f04ab39880aec9b51b73421
felipe-crosa <99428732+felipe-crosa@users.noreply.github.com>
2022-03-30 14:06:51 -0300

    [cu 2a7rgn9] User Registration (#2)
    * User Registration functionality
    
    * Data providers for tests
    
    * Make email validation strict
    
    * Reduce github load time
    
    * Envcypting password
    
    Co-authored-by: Felipe Crosa <felipe@lightit.com>

---
+++ tests/Feature/UserRegistrationTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserRegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_is_registered_on_database()
+    {
+        $data = [
+            'name' => 'Felipe',
+            'email' => 'felicrosa@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ];
+
+        $response = $this->postJson('/api/register', $data);
+
+        $response->assertSuccessful();
+
+        $this->assertDatabaseHas('users', ['name' => 'Felipe', 'email' => 'felicrosa@gmail.com']);
+
+        $response->assertJson(['status'=>200, 'message'=>'User has been added succesfully']);
+    }
+
+    /**
+     * @dataProvider  invalidUserDataProvider
+     */
+    public function test_user_cant_register_with_invalid_data($user)
+    {
+        $response = $this->postJson('/api/register', $user);
+        $response->assertStatus(422);
+    }
+
+    public function invalidUserDataProvider(): array
+    {
+        return [
+            ['no Name'=>[
+                'email' => 'felicrosa@gmail.com',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+            ]],
+            ['no Password'=>[
+                'name' => 'Felipe',
+                'email' => 'felicrosa@gmail.com',
+            ]],
+            ['no Password Confirmation'=>[
+                'name' => 'Felipe',
+                'email' => 'felicrosa@gmail.com',
+                'password' => '12345678',
+            ]],
+            ['wrong Password Confirmation'=>[
+                'name' => 'Felipe',
+                'email' => 'felicrosa@gmail.com',
+                'password' => '12345678',
+                'password_confirmation' => '1234545678',
+            ]],
+            ['no email'=>[
+                'name' => 'Felipe',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+            ]],
+            ['invalid email'=>[
+                'name' => 'Felipe',
+                'email' => 'felicrosamail.com',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+            ]],
+            ['invalid name'=>[
+                'name' => 'Fel4ipe',
+                'email' => 'felicrosa@gmail.com',
+                'password' => '12345678',
+                'password_confirmation' => '12345678',
+            ]],
+            ['shortPassword'=>[
+                'name' => 'Felipe',
+                'email' => 'felicrosa@gmail.com',
+                'password' => '1454',
+                'password_confirmation' => '1454',
+            ]],
+        ];
+    }
+}
