caf5b1fc820a7da2a79731c57709bad780ad0d51
Felipe Crosa <felipe@lightit.com>
2022-03-30 14:08:01 -0300

    Routes.api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+
+
 Route::post('/login', \App\Http\Controllers\UserLoginController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
