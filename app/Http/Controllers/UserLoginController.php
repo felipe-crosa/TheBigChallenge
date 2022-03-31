b6b15093a7565f607d1c8049735846d040b4cc25
felipe-crosa <99428732+felipe-crosa@users.noreply.github.com>
2022-03-31 09:34:20 -0300

    Created User Resource and applied it on login return (#4)
    * Created User Resource and applied it on login return
    
    * Replaced response with UserResource
    
    * Removed unnecesary comment
    
    * Cleaned up Login Controller
    
    Co-authored-by: Felipe Crosa <felipe@lightit.com>

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -3,32 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
 class UserLoginController extends Controller
 {
-    public function __invoke(UserLoginRequest $request) : JsonResponse
+    public function __invoke(UserLoginRequest $request): UserResource|JsonResponse
     {
         $arguments = $request->validated();
 
         $user = User::where('email', $arguments['email'])->first();
-        $response = [];
-        if (isset($user)) {
-            if (Hash::check($arguments['password'], $user->password)) {
-                $response = [
-                    'status'=>200,
-                    'message'=>'User logged in succesfully',
-                    'token'=>$user->createToken('app')->plainTextToken,
-                    'user'=>$user,
-                ];
-            } else {
-                $response = ['status'=>401, 'message'=>'Invalid credentials'];
-            }
-        } else {
-            $response = ['status'=>401, 'message'=>'Invalid credentials'];
+
+        if ($user && Hash::check($arguments['password'], $user->password)) {
+            return (new UserResource($user))
+                ->additional([
+                    'status' => 200,
+                    'message' => 'User logged in succesfully',
+                    'token' => $user->createToken('app')->plainTextToken,
+                ]);
         }
+
+        $response = ['status' => 401, 'message' => 'Invalid credentials'];
 
         return response()->json($response);
     }
