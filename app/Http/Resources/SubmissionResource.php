df6fe87904f81e3fcbea91bbd0b35ae7f4055b60
Felipe Crosa <felipe@lightit.com>
2022-04-19 13:58:34 -0300

    Improved SubmissionResource

--- app/Http/Resources/SubmissionResource.php
+++ app/Http/Resources/SubmissionResource.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\DoctorInformation;
-use App\Models\PatientInformation;
 use App\Models\Submission;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,17 +12,10 @@ class SubmissionResource extends JsonResource
 {
     public function toArray($request): array
     {
-        $patient = PatientInformation::find($this->patient_id);
-        $doctor = $this->doctor_id;
-        if ($doctor) {
-            $doctor = DoctorInformation::find($this->doctor_id);
-            $doctor = new DoctorInformationResource($doctor);
-        }
-
         return [
             'id' => $this->id,
-            'patient' => new PatientInformationResource($patient),
-            'doctor' => $doctor,
+            'patient' => ($this->patient_id) ? new PatientInformationResource($this->patient) : $this->patient_id,
+            'doctor' => ($this->doctor_id) ? new DoctorInformationResource($this->doctor) : $this->doctor_id,
             'symptoms' => $this->symptoms,
             'observations' => $this->observations,
             'diagnosis' => $this->diagnosis,
