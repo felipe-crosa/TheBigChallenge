7385e7fe6381597235f7ed0d9ea01f6ecc1628c4
Felipe Crosa <felipe@lightit.com>
2022-04-27 15:05:44 -0300

    Fixes

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/email/verify/{id}/{hash}', \App\Http\Controllers\VerifyEmailController::class)->name('verification.verify');
 
-Route::group(['middleware' => 'guest'], function () {
+Route::group(['middleware' => 'guest:sanctum'], function () {
     Route::post('/register', \App\Http\Controllers\UserRegistrationController::class);
     Route::post('/login', \App\Http\Controllers\UserLoginController::class);
 });
@@ -25,20 +25,23 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::middleware('auth:sanctum')->post('/logout', \App\Http\Controllers\UserLogOutController::class);
     Route::get('/submissions/{submission}', \App\Http\Controllers\GetSubmissionController::class);
     Route::get('/submissions', \App\Http\Controllers\ListSubmissionsController::class);
+
+    Route::group(['middleware' => ['role:doctor']], function () {
+        Route::patch('/submissions/{submission}/assign', \App\Http\Controllers\AssignSubmissionController::class);
+        Route::post('/createDoctorInformation', \App\Http\Controllers\CreateDoctorInformationController::class);
+        Route::get('/getDoctorInformation', \App\Http\Controllers\GetDoctorInformationController::class);
+        Route::patch('/updateDoctorInformation', \App\Http\Controllers\UpdateDoctorInformationController::class);
+        Route::post('/submissions/{submission}/diagnose', \App\Http\Controllers\UploadDiagnosisController::class);
+        Route::delete('/submissions/{submission}/deleteDiagnosis', \App\Http\Controllers\DeleteDiagnosisController::class);
+    });
+
+    Route::group(['middleware' => ['role:patient']], function () {
+        Route::post('/createPatientInformation', \App\Http\Controllers\CreatePatientInformationController::class);
+        Route::patch('/updatePatientInformation', \App\Http\Controllers\UpdatePatientInformationController::class);
+        Route::get('/getPatientInformation', \App\Http\Controllers\GetPatientInformationController::class);
+        Route::post('/createSubmission', \App\Http\Controllers\CreateSubmissionController::class);
+        Route::delete('/submissions/{submission}/delete', \App\Http\Controllers\DeleteSubmissionController::class);
+        Route::patch('/submissions/{submission}/update', \App\Http\Controllers\UpdateSubmissionController::class);
+    });
 });
 
-Route::group(['middleware' => ['role:doctor']], function () {
-    Route::post('/createDoctorInformation', \App\Http\Controllers\CreateDoctorInformationController::class);
-    Route::get('/getDoctorInformation', \App\Http\Controllers\GetDoctorInformationController::class);
-    Route::patch('/updateDoctorInformation', \App\Http\Controllers\UpdateDoctorInformationController::class);
-    Route::patch('/submissions/{submission}/assign', \App\Http\Controllers\AssignSubmissionController::class);
-});
-
-Route::group(['middleware' => ['role:patient']], function () {
-    Route::post('/createPatientInformation', \App\Http\Controllers\CreatePatientInformationController::class);
-    Route::patch('/updatePatientInformation', \App\Http\Controllers\UpdatePatientInformationController::class);
-    Route::get('/getPatientInformation', \App\Http\Controllers\GetPatientInformationController::class);
-    Route::post('/createSubmission', \App\Http\Controllers\CreateSubmissionController::class);
-    Route::delete('/submissions/{submission}/delete', \App\Http\Controllers\DeleteSubmissionController::class);
-    Route::patch('/submissions/{submission}/update', \App\Http\Controllers\UpdateSubmissionController::class);
-});
