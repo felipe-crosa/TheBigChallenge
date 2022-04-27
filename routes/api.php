b971d763f02eb5035a7bb9d504e26b0e7dd50181
felipe-crosa <felipe-crosa@users.noreply.github.com>
2022-04-27 18:32:34 +0000

    Fix styling

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::delete('/submissions/{submission}/deleteDiagnosis', \App\Http\Controllers\DeleteDiagnosisController::class);
     });
 
-
     Route::group(['middleware' => ['role:patient']], function () {
         Route::post('/createPatientInformation', \App\Http\Controllers\CreatePatientInformationController::class);
         Route::patch('/updatePatientInformation', \App\Http\Controllers\UpdatePatientInformationController::class);
